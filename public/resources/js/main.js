e2715cf3b93f4a9520a9dabfa746848f6af6712c
Luke Kw <luke92pl@gmail.com>
2015-11-16 20:35:29 +0100

    rebuild of structure

---
+++ public/resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+// $.getJSON("api.openweathermap.org/data/2.5/weather?q=London", function(json) {
+// 	$(".weather-panel").html(json.name)
+// })
+});
+
+/*
+
+latitude: 50.163702699999995
+longitude: 19.3003386
+
+*/
